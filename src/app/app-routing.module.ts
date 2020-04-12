539c8dea927d8f84a9ae9324e57681ca33162107
jofred <jofred.reinosa@haintech.cl>
2020-04-12 10:39:15 -0400

    add linechart component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LinechartComponent } from './linechart/linechart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'linechart', component: LinechartComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
